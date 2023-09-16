4f58aa3632ec0f40861261cbfd8de51a05e55c2e
Manjunath-Aradhya <manjunath.r.sgz@gmail.com>
2023-09-16 22:09:43 +0530

    Personal portfolio

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <Navbar expand="lg" bg="dark" variant="dark" fixed="top">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Your Name</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+export default function Home() {
+  return (
+    <Container
+    className="gradient-background"
+    fluid
+    style={{
+      background: 'linear-gradient(to bottom,#2096ff,#05ffa3)', 
+      minHeight: '100vh', // Adjust this to make the gradient cover the entire viewport
+    }}
+  >
+    {/* Your content goes here */}
+   
+  </Container>
+  )
+}
